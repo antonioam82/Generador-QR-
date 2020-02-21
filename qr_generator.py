ec35bfb4156dd20d854f2497bc5b9fb065417e64
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-02-21 19:08:08 +0100

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -5,8 +5,10 @@ ventana = Tk()
 ventana.title("QR Code Generator")
 ventana.configure(background="light blue")
 ventana.geometry("750x250")
+input_text=StringVar()
 
-Entry(ventana,font=('Arial',15,"bold"),width=45,justify="left").place(x=131,y=107)
+Entry(ventana,font=('Arial',15,"bold"),width=45,justify="left",textvariable=input_text).place(x=131,y=107)
 Button(ventana,text="CREAR CÓDIGO",bg="light green").place(x=330,y=175)
 
 ventana.mainloop()
+
