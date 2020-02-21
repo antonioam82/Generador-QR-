cfb71a73af643ffd462004bed9a9ed53548d69d8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-02-21 19:54:45 +0100

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -12,7 +12,7 @@ def create_code():
         data = input_text.get()
         img = qrcode.make(data)
         img.save("my_qrcode.png")
-
+Label(ventana,text="RUTA O DIRECCIÓN",bg="light blue").place(x=322,y=85)
 Entry(ventana,font=('Arial',15),width=45,justify="left",textvariable=input_text).place(x=131,y=107)
 Button(ventana,text="CREAR CÓDIGO",bg="light green",command=create_code).place(x=330,y=175)
 
