8d5f06955f867e98b20e44d0ace828f39a8f84f0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-02-21 19:01:48 +0100

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -7,5 +7,6 @@ ventana.configure(background="light blue")
 ventana.geometry("750x250")
 
 Entry(ventana,font=('Arial',15,"bold"),width=45,justify="left").place(x=131,y=107)
+Button(ventana,text="CREAR CÓDIGO",bg="light green").place(x=330,y=175)
 
 ventana.mainloop()
