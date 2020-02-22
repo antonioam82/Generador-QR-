e6d904714a961003e383cb77001b77b9b8e55db9
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-02-22 19:27:47 +0100

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import qrcode
+import threading
 
 ventana = Tk()
 ventana.title("QR Code Generator")
@@ -7,14 +8,19 @@ ventana.configure(background="light blue")
 ventana.geometry("750x250")
 input_text=StringVar()
 
+def inicia():
+    t = threading.Thread(target=create_code)
+    t.start()
+
 def create_code():
     if input_text.get()!="":
         data = input_text.get()
         img = qrcode.make(data)
         img.save("my_qrcode.png")
+        
 Label(ventana,text="RUTA O DIRECCIÓN",bg="light blue").place(x=322,y=85)
 Entry(ventana,font=('Arial',15),width=45,justify="left",textvariable=input_text).place(x=131,y=107)
-Button(ventana,text="CREAR CÓDIGO",bg="light green",command=create_code).place(x=330,y=175)
+Button(ventana,text="CREAR CÓDIGO",bg="light green",command=inicia).place(x=330,y=175)
 
 ventana.mainloop()
 
