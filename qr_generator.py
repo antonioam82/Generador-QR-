9ef2c55bcfdd16f57f7fb01ce4550fce99a292a5
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-02-26 16:58:41 +0100

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -1,36 +1,60 @@
+import tkinter
 from tkinter import *
-from tkinter import messagebox
+from tkinter import ttk
+
+root = tkinter.Tk()
+color = "light blue"
+nb = ttk.Notebook(width=750, height=250)
+input_text=StringVar()
+nb.pressed_index = None
+
+f1 = tkinter.Frame(nb, background=color)
+f2 = tkinter.Frame(nb, background=color)
+f3 = tkinter.Frame(nb, background=color)
+Entry(f1,font=('Arial',15),width=45,justify="left",textvariable=input_text).place(x=131,y=97)
+Button(f1,text="CREAR CÓDIGO",fg="black",bg="light green").place(x=330,y=174)
+
+nb.add(f1, text='WEB', padding=3)
+nb.add(f2, text='TEXTO', padding=3)
+nb.add(f3, text='IMAGEN', padding=3)
+nb.pack(expand=1, fill='both')
+
+root.mainloop()
+
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox, ttk
 import qrcode
 import threading
 
-ventana = Tk()
-ventana.title("QR Code Generator")
-ventana.configure(background="light blue")
-ventana.geometry("750x250")
-input_text=StringVar()
+#ventana = Tk()
+#ventana.title("QR Code Generator")
+#ventana.configure(background="light blue")
+#ventana.geometry("750x250")
+#input_text=StringVar()
 
-def inicia():
-    t = threading.Thread(target=create_code)
-    t.start()
+#def inicia():
+    #t = threading.Thread(target=create_code)
+    #t.start()
 
-def create_code():
-    global img, size
-    if input_text.get()!="":
-        try:
-            data = input_text.get()
-            img = qrcode.make(data)
-            img.save("my_qrcode.png")
-            messagebox.showinfo("QR CREADO","Código creado con éxito")
-        except:
-            messagebox.showwarning("ERROR","HUBO UN PROBLEMA AL GENERAR EL CÓDIGO")
-    else:
-        messagebox.showwarning("ERROR","Elemento no especificado")
+#def create_code():
+    #global img, size
+    #if input_text.get()!="":
+        #try:
+            #data = input_text.get()
+            #img = qrcode.make(data)
+            #img.save("my_qrcode.png")
+            #messagebox.showinfo("QR CREADO","Código creado con éxito")
+        #except:
+            #messagebox.showwarning("ERROR","HUBO UN PROBLEMA AL GENERAR EL CÓDIGO")
+    #else:
+        #messagebox.showwarning("ERROR","Elemento no especificado")
         
-Label(ventana,text="RUTA O DIRECCIÓN",bg="light blue").place(x=322,y=85)
-Entry(ventana,font=('Arial',15),width=45,justify="left",textvariable=input_text).place(x=131,y=107)
-Button(ventana,text="CREAR CÓDIGO",bg="light green",command=inicia).place(x=330,y=175)
+#Label(ventana,text="RUTA O DIRECCIÓN",bg="light blue").place(x=322,y=85)
+#Entry(ventana,font=('Arial',15),width=45,justify="left",textvariable=input_text).place(x=131,y=107)
+#Button(ventana,text="CREAR CÓDIGO",bg="light green",command=inicia).place(x=330,y=175)
 
-ventana.mainloop()
+#ventana.mainloop()
 
 
 
