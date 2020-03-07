08010d1283baad56e3983ecdc27cba9815f561a7
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-07 18:39:01 +0100

    Update QR_Generator.py

--- QR_Generator.py
+++ QR_Generator.py
@@ -9,6 +9,10 @@ import cv2
 import threading
 import os
 
+def estado_ver(s):
+    for i in btv:
+        i.configure(state=s)
+
 def create_data(ti):
     global data, nom_archiv
     if ti == "w":
@@ -28,23 +32,26 @@ def create_code():
             img = qrcode.make(data)
             img.save(nom_archiv)
             messagebox.showinfo("QR CREADO","Código creado con éxito")
-            data = ""
+            estado_ver('normal')
+            #data = ""
         else:
             messagebox.showwarning("SIN CONTENIDO","NO SE INTRODUJERON DATOS")
-            nom_archiv = ""
+            estado_ver('disabled')
+            #nom_archiv = ""
     except:
         messagebox.showwarning("ERROR","HUBO UN PROBLEMA AL GENERAR EL CÓDIGO")
+    
 
 def ver_codigo():
-    if nom_archiv != "":
-        print(nom_archiv)
-        im = cv2.imread(nom_archiv)
-        cv2.imshow("Ultimo QR creado",im)
+    print(nom_archiv)
+    im = cv2.imread(nom_archiv)
+    cv2.imshow("Ultimo QR creado",im)
 
 def abrir_archivo(ex,n):
     global data, nom_archiv, file
     nom_archiv = ""
     data = ""
+    estado_ver('disabled')
     for i in label_file:
         i.configure(text="NINGÚN ELEMENTO SELECCIONADO")
     ruta = filedialog.askopenfilename(initialdir = "/",
@@ -98,6 +105,8 @@ Entry(f1,font=('Arial',15),width=45,justify="left",textvariable=input_text).plac
 Button(f1,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('w')).place(x=330,y=174)
 etiFormato1=Label(f1,text=texto_formato,bg="light blue")
 etiFormato1.place(x=751,y=66)#780
+btnVer1 = Button(f1,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer1.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f2"
 display=scrolledtext.ScrolledText(f2,width=66,foreground='black',height=1,padx=10, pady=10,font=('Arial', 10))
 display.place(x=131,y=97)
@@ -105,6 +114,8 @@ Label(f2,text="TEXTO:",bg="light blue").place(x=88,y=95)
 Button(f2,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('t')).place(x=330,y=174)
 etiFormato2=Label(f2,text=texto_formato,bg="light blue")
 etiFormato2.place(x=751,y=66)
+btnVer2 = Button(f2,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer2.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f3"
 Button(f3,text="BUSCAR PNG",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("png",0)).place(x=321,y=130)
 Button(f3,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
@@ -112,6 +123,8 @@ etiElemen1=Label(f3,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80
 etiElemen1.place(x=97,y=70)
 etiFormato3=Label(f3,text=texto_formato,bg="light blue")
 etiFormato3.place(x=751,y=66)
+btnVer3 = Button(f3,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer3.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f4"
 Button(f4,text="BUSCAR JPG",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("jpg",1)).place(x=321,y=130)
 Button(f4,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
@@ -119,6 +132,8 @@ etiElemen2=Label(f4,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80
 etiElemen2.place(x=97,y=70)
 etiFormato4=Label(f4,text=texto_formato,bg="light blue")
 etiFormato4.place(x=751,y=66)
+btnVer4 = Button(f4,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer4.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f5"
 Button(f5,text="BUSCAR MP3",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("mp3",2)).place(x=321,y=130)
 Button(f5,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
@@ -126,6 +141,8 @@ etiElemen3=Label(f5,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80
 etiElemen3.place(x=97,y=70)
 etiFormato5=Label(f5,text=texto_formato,bg="light blue")
 etiFormato5.place(x=751,y=66)
+btnVer5 = Button(f5,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer5.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f6"
 Button(f6,text="BUSCAR PDF",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("pdf",3)).place(x=321,y=130)
 Button(f6,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
@@ -133,6 +150,8 @@ etiElemen4=Label(f6,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80
 etiElemen4.place(x=97,y=70)
 etiFormato6=Label(f6,text=texto_formato,bg="light blue")
 etiFormato6.place(x=751,y=66)
+btnVer6 = Button(f6,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer6.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f7"
 Button(f7,text="BUSCAR VIDEO",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("mp4",4)).place(x=321,y=130)
 Button(f7,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
@@ -140,15 +159,18 @@ etiElemen5=Label(f7,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80
 etiElemen5.place(x=97,y=70)
 etiFormato7=Label(f7,text=texto_formato,bg="light blue")
 etiFormato7.place(x=751,y=66)
+btnVer7 = Button(f7,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer7.place(x=754,y=174)
 
 bts = [etiFormato1,etiFormato2,etiFormato3,etiFormato4,etiFormato5,etiFormato6,etiFormato7]
 label_file = [etiElemen1,etiElemen2,etiElemen3,etiElemen4,etiElemen5]
 pestas = [f1,f2,f3,f4,f5,f6,f7]
+btv = [btnVer1,btnVer2,btnVer3,btnVer4,btnVer5,btnVer6,btnVer7]
 
 for i in pestas:
     Button(i,text="PNG",width=15,bg="light green",command=lambda:cambia_formato('.png','FORMATO: PNG')).place(x=754,y=97)
     Button(i,text="JPG",width=15,bg="light green",command=lambda:cambia_formato('.jpg','FORMATO: JPG')).place(x=754,y=130)
-    Button(i,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo).place(x=754,y=174)
+    
 
 nb.add(f1, text='WEB', padding=3)
 nb.add(f2, text='TEXTO', padding=3)
