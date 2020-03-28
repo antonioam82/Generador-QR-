19f5563bf6a09b0d7bebd5964ce7cb7f25a9cdcf
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-28 16:33:37 +0100

    Update QR_GeneratorB.py

--- QR_GeneratorB.py
+++ QR_GeneratorB.py
@@ -1,70 +1,61 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import tkinter
+from segno import helpers
+from unidecode import unidecode
 from tkinter import *
 from tkinter import messagebox, ttk, filedialog
 import tkinter.scrolledtext as scrolledtext
-from unidecode import unidecode
 import qrcode
-import glob
 import cv2
 import threading
 import os
 
+def guarda_en():
+    global archivoGuardar
+    archivoGuardar=filedialog.asksaveasfilename(initialdir="/",title="Guardar en",defaultextension=formato)
+    return archivoGuardar
+    
 def estado_ver(s):
     for i in btv:
         i.configure(state=s)
 
-def uni_name(n):
-    files = glob.glob('*'+formato)
-    count = 0
-    for i in files:
-        if n in i:
-            count+=1
-    if count>0:
-        return n+"("+str(count)+")"+formato
-    else:
-        return n+formato
-
 def create_data(ti):
-    global data, nom_archiv
+    global data, nom_archiv, vcard
     if ti == "w":
         data = unidecode(input_text.get())
-        nom_archiv = uni_name("web_qrcode")
     elif ti == "t":
-        data = unidecode(display.get('1.0',END))############################
-        nom_archiv = uni_name("text_qrcode")
-    elif ti == "m" and file != "":
-        nom_archiv = file+"_qrcode"+formato
-
+        data = unidecode(display.get('1.0',END))
+    
 def create_code():
-    global data
-    #print(data)
+    global data, archi, vcard
     try:
         if data != "":
             img = qrcode.make(data)
-            img.save(nom_archiv)
-            messagebox.showinfo("QR CREADO","Código creado con éxito")
-            estado_ver('normal')
-            #data = ""
-        else:
-            messagebox.showwarning("SIN CONTENIDO","NO SE INTRODUJERON DATOS")
-            estado_ver('disabled')
-            #nom_archiv = ""
+            archi = guarda_en()
+            if archi != "": #PROVISIONAL
+                img.save(archi)
+                messagebox.showinfo("QR CREADO","Código creado con éxito")
+                estado_ver('normal')
+            else:
+                messagebox.showwarning("SIN CONTENIDO","NO SE INTRODUJERON DATOS")
+                estado_ver('disabled')
     except:
         messagebox.showwarning("ERROR","HUBO UN PROBLEMA AL GENERAR EL CÓDIGO")
-
-
+    
 def ver_codigo():
-    if nom_archiv != "":
-        print(nom_archiv)
-        im = cv2.imread(nom_archiv)
-        cv2.imshow("Your QR Code",im)
+    try:
+        im = cv2.imread(archi)
+        cv2.imshow(archivoGuardar.split("/")[-1],im)
+    except:
+        messagebox.showwarning("ERROR","HUBO UN PROBLEMA AL MOSTRAR EL CÓDIGO")
 
 def abrir_archivo(ex,n):
-    global data, nom_archiv,file
+    global data, nom_archiv, file
     nom_archiv = ""
     data = ""
+    estado_ver('disabled')
     for i in label_file:
         i.configure(text="NINGÚN ELEMENTO SELECCIONADO")
     ruta = filedialog.askopenfilename(initialdir = "/",
@@ -74,13 +65,11 @@ def abrir_archivo(ex,n):
         lista_ruta = ruta.split("/")
         data = str(lista_ruta[-1])
         file,ex=os.path.splitext(data)
-        file=unidecode(file)
-        #lis_nd = "/".join(lista_ruta[:-1])
-        #os.chdir(lis_nd)
+        file = unidecode(file)
         label_file[n].configure(text="ELEMENTO SELECCIONADO: "+data)
 
-def inicia(t):
-    create_data(t)
+def inicia(ti):
+    create_data(ti)
     t = threading.Thread(target=create_code)
     t.start()
 
@@ -96,12 +85,12 @@ root.title("QR Code Generator")
 color = "light blue"
 nb = ttk.Notebook(width=997, height=250)#765
 input_text=StringVar()
-input_text2=StringVar()
-nb.pressed_index = None
+#nb.pressed_index = None
 formato = ".png"
 texto_formato = "FORMATO: PNG"
 data = ""
-nom_archiv = ""
+file = ""
+archi = ""
 
 f1 = tkinter.Frame(nb, background=color)
 f2 = tkinter.Frame(nb, background=color)
@@ -110,6 +99,7 @@ f4 = tkinter.Frame(nb, background=color)
 f5 = tkinter.Frame(nb, background=color)
 f6 = tkinter.Frame(nb, background=color)
 f7 = tkinter.Frame(nb, background=color)
+f8 = tkinter.Frame(nb, background=color)
 
 #ELEMENTOS PESTAÑA "f1"
 Label(f1,text="DIRECCIÓN WEB",bg="light blue").place(x=331,y=74)
@@ -131,7 +121,7 @@ btnVer2.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f3"
 Button(f3,text="BUSCAR PNG",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("png",0)).place(x=321,y=130)
 Button(f3,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
-etiElemen1=Label(f3,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80)
+etiElemen1=Label(f3,text="NINGÚN ELEMENTO SELECCIONADO",bg="light blue",width=80)
 etiElemen1.place(x=97,y=70)
 etiFormato3=Label(f3,text=texto_formato,bg="light blue")
 etiFormato3.place(x=751,y=66)
@@ -140,7 +130,7 @@ btnVer3.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f4"
 Button(f4,text="BUSCAR JPG",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("jpg",1)).place(x=321,y=130)
 Button(f4,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
-etiElemen2=Label(f4,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80)
+etiElemen2=Label(f4,text="NINGÚN ELEMENTO SELECCIONADO",bg="light blue",width=80)
 etiElemen2.place(x=97,y=70)
 etiFormato4=Label(f4,text=texto_formato,bg="light blue")
 etiFormato4.place(x=751,y=66)
@@ -149,7 +139,7 @@ btnVer4.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f5"
 Button(f5,text="BUSCAR MP3",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("mp3",2)).place(x=321,y=130)
 Button(f5,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
-etiElemen3=Label(f5,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80)
+etiElemen3=Label(f5,text="NINGÚN ELEMENTO SELECCIONADO",bg="light blue",width=80)
 etiElemen3.place(x=97,y=70)
 etiFormato5=Label(f5,text=texto_formato,bg="light blue")
 etiFormato5.place(x=751,y=66)
@@ -158,7 +148,7 @@ btnVer5.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f6"
 Button(f6,text="BUSCAR PDF",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("pdf",3)).place(x=321,y=130)
 Button(f6,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
-etiElemen4=Label(f6,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80)
+etiElemen4=Label(f6,text="NINGÚN ELEMENTO SELECCIONADO",bg="light blue",width=80)
 etiElemen4.place(x=97,y=70)
 etiFormato6=Label(f6,text=texto_formato,bg="light blue")
 etiFormato6.place(x=751,y=66)
@@ -167,22 +157,31 @@ btnVer6.place(x=754,y=174)
 #ELEMENTOS PESTAÑA "f7"
 Button(f7,text="BUSCAR VIDEO",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("mp4",4)).place(x=321,y=130)
 Button(f7,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
-etiElemen5=Label(f7,text="NINGÚN ELEMENTO SELECIONADO",bg="light blue",width=80)
+etiElemen5=Label(f7,text="NINGÚN ELEMENTO SELECCIONADO",bg="light blue",width=80)
 etiElemen5.place(x=97,y=70)
 etiFormato7=Label(f7,text=texto_formato,bg="light blue")
 etiFormato7.place(x=751,y=66)
 btnVer7 = Button(f7,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
 btnVer7.place(x=754,y=174)
+#ELEMNTOS PESTAÑA "f8"
+Button(f8,text="BUSCAR GIF",fg="black",width=15,bg="light green",command=lambda:abrir_archivo("gif",5)).place(x=321,y=130)
+Button(f8,text="CREAR CÓDIGO",fg="black",bg="light green",command=lambda:inicia('m')).place(x=330,y=174)
+etiElemen6=Label(f8,text="NINGÚN ELEMENTO SELECCIONADO",bg="light blue",width=80)
+etiElemen6.place(x=97,y=70)
+etiFormato8=Label(f8,text=texto_formato,bg="light blue")
+etiFormato8.place(x=751,y=66)
+btnVer8 = Button(f8,text="VER CÓDIGO",bg="gold2",width=15,command=ver_codigo,state='disabled')
+btnVer8.place(x=754,y=174)
 
-bts = [etiFormato1,etiFormato2,etiFormato3,etiFormato4,etiFormato5,etiFormato6,etiFormato7]
-label_file = [etiElemen1,etiElemen2,etiElemen3,etiElemen4,etiElemen5]
-pestas = [f1,f2,f3,f4,f5,f6,f7]
-btv = [btnVer1,btnVer2,btnVer3,btnVer4,btnVer5,btnVer6,btnVer7]
+bts = [etiFormato1,etiFormato2,etiFormato3,etiFormato4,etiFormato5,etiFormato6,etiFormato7,etiFormato8]
+label_file = [etiElemen1,etiElemen2,etiElemen3,etiElemen4,etiElemen5,etiElemen6]
+pestas = [f1,f2,f3,f4,f5,f6,f7,f8]
+btv = [btnVer1,btnVer2,btnVer3,btnVer4,btnVer5,btnVer6,btnVer7,btnVer8]
 
 for i in pestas:
     Button(i,text="PNG",width=15,bg="light green",command=lambda:cambia_formato('.png','FORMATO: PNG')).place(x=754,y=97)
     Button(i,text="JPG",width=15,bg="light green",command=lambda:cambia_formato('.jpg','FORMATO: JPG')).place(x=754,y=130)
-
+    
 nb.add(f1, text='WEB', padding=3)
 nb.add(f2, text='TEXTO', padding=3)
 nb.add(f3, text='PNG', padding=3)
@@ -190,6 +189,7 @@ nb.add(f4, text='JPG',padding=3)
 nb.add(f5, text='MP3',padding=3)
 nb.add(f6, text='PDF',padding=3)
 nb.add(f7, text='MP4',padding=3)
+nb.add(f8, text='GIF',padding=3)
 nb.pack(expand=1, fill='both')
 
 root.mainloop()
