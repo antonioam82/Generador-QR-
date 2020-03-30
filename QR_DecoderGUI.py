2f4f2ed923692efe70dc95accad3b8d9e42646e2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-30 19:28:33 +0200

    Create QR_DecoderGUI.py

---
+++ QR_DecoderGUI.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from pyzbar.pyzbar import decode
+import cv2
+import os
+
+ventana = Tk()
+ventana.title('LECTOR DE CÓDIGOS QR')
+ventana.geometry("500x220")
+
+ventana.mainloop()
