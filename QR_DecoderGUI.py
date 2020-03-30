b79731c9f5f9ab6a78691a2223b572d741d3d140
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-30 19:36:11 +0200

    Update QR_DecoderGUI.py

--- QR_DecoderGUI.py
+++ QR_DecoderGUI.py
@@ -7,4 +7,7 @@ ventana = Tk()
 ventana.title('LECTOR DE CÓDIGOS QR')
 ventana.geometry("500x220")
 
+etiElement = Label(ventana, text="CARGAR ARCHIVO PNG O JPG",width=68)
+etiElement.place(x=9,y=70)
+
 ventana.mainloop()
