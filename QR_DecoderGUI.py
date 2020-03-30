e92b3d6b467d4a5077993adc259cb8cacf0a21fb
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-30 19:49:36 +0200

    Update QR_DecoderGUI.py

--- QR_DecoderGUI.py
+++ QR_DecoderGUI.py
@@ -8,9 +8,11 @@ ventana.title('LECTOR DE CÓDIGOS QR')
 ventana.geometry("500x220")
 
 etiElement = Label(ventana, text="CARGAR ARCHIVO PNG O JPG",width=68)
-etiElement.place(x=9,y=70)
+etiElement.place(x=9,y=65)
 
-btnCargar = Button(ventana, text="CARGAR CÓDIGO",bg="khaki")
-btnCargar.place(x=195,y=110)
+btnCargar = Button(ventana, text="CARGAR CÓDIGO",bg="khaki",width=22)
+btnCargar.place(x=169,y=110)
+btnScreen = Button(ventana, text="DETECTAR QR EN PANTALLA",bg="khaki")
+btnScreen.place(x=169,y=155)
 
 ventana.mainloop()
