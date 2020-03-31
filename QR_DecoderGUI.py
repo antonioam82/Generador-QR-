57720f41d5a86339607e36c89de6654a3d799588
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-31 18:42:59 +0200

    Update QR_DecoderGUI.py

--- QR_DecoderGUI.py
+++ QR_DecoderGUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox, filedialog
+from unidecode import unidecode
 from pyzbar.pyzbar import decode
 import pyautogui
 import cv2
@@ -9,13 +10,18 @@ def abrir():
     ruta = filedialog.askopenfilename(initialdir="/",title="SELECCIONAR ARCHIVO",
                        filetypes =(("png files","*.png") ,("jpg files","*.jpg")))
     if ruta != "":
-        elemento = ruta.split("/")[-1]
+        archivo = cv2.imread(ruta)
+        info = decode(archivo)
+        if info != []:
+            etiElement.configure(text="ELEMENTO INDENTIFICADO: "+str(info[0][0]))
+        else:
+            etiElement.configure(text="NO SE DETECTO CÓDIGO")
 
 ventana = Tk()
 ventana.title('LECTOR DE CÓDIGOS QR')
-ventana.geometry("500x220")
+ventana.geometry("520x220")
 
-etiElement = Label(ventana, text="CARGAR ARCHIVO PNG O JPG",width=68)
+etiElement = Label(ventana, text="CARGAR ARCHIVO PNG O JPG",width=71)
 etiElement.place(x=9,y=65)
 
 btnCargar = Button(ventana, text="CARGAR CÓDIGO",bg="khaki",width=22,command=abrir)
@@ -24,3 +30,4 @@ btnScreen = Button(ventana, text="DETECTAR QR EN PANTALLA",bg="khaki")
 btnScreen.place(x=169,y=155)
 
 ventana.mainloop()
+
