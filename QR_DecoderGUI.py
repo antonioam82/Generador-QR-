57e3278b4b8af03a2f2da3ca50b245f341417ade
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-03-31 19:11:34 +0200

    Update QR_DecoderGUI.py

--- QR_DecoderGUI.py
+++ QR_DecoderGUI.py
@@ -6,6 +6,15 @@ import pyautogui
 import cv2
 import os
 
+def screen_shoot():
+    pyautogui.screenshot("screenshoot.jpg")
+    archivo = cv2.imread("screenshoot.jpg")
+    info = decode(archivo)
+    if info != []:
+        etiElement.configure(text="ELEMENTO INDENTIFICADO: "+str(info[0][0]))
+    else:
+        etiElement.configure(text="NO SE DETECTO CÓDIGO")
+
 def abrir():
     ruta = filedialog.askopenfilename(initialdir="/",title="SELECCIONAR ARCHIVO",
                        filetypes =(("png files","*.png") ,("jpg files","*.jpg")))
@@ -20,13 +29,14 @@ def abrir():
 ventana = Tk()
 ventana.title('LECTOR DE CÓDIGOS QR')
 ventana.geometry("520x220")
+file_name=""
 
 etiElement = Label(ventana, text="CARGAR ARCHIVO PNG O JPG",width=71)
 etiElement.place(x=9,y=65)
 
 btnCargar = Button(ventana, text="CARGAR CÓDIGO",bg="khaki",width=22,command=abrir)
 btnCargar.place(x=178,y=110)
-btnScreen = Button(ventana, text="DETECTAR QR EN PANTALLA",bg="khaki")
+btnScreen = Button(ventana, text="DETECTAR QR EN PANTALLA",bg="khaki",command=screen_shoot)
 btnScreen.place(x=178,y=155)
 
 ventana.mainloop()
