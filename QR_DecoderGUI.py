aa1fce99173aa43f824119b9ac495b112fe867f0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-04-04 20:21:18 +0200

    Update QR_DecoderGUI.py

--- QR_DecoderGUI.py
+++ QR_DecoderGUI.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox, filedialog
+import tkinter.scrolledtext as scrolledtext
 from unidecode import unidecode
+import threading
 from pyzbar.pyzbar import decode
 import pyautogui
 import cv2
@@ -11,9 +13,9 @@ def screen_shoot():
     archivo = cv2.imread("screenshoot.jpg")
     info = decode(archivo)
     if info != []:
-        etiElement.configure(text="ELEM INDENT: "+str(info[0][0]))
+        display.insert(END,info[0][0])
     else:
-        etiElement.configure(text="NO SE DETECTO CÓDIGO")
+        messagebox.showwarning("ERROR","NO SE DETECTÓ CÓDIGO")
     os.remove("screenshoot.jpg")
 
 def abrir():
@@ -23,9 +25,13 @@ def abrir():
         archivo = cv2.imread(ruta)
         info = decode(archivo)
         if info != []:
-            etiElement.configure(text="ELEM INDENT: "+str(info[0][0]))
+            display.insert(END,info[0][0])
         else:
             etiElement.configure(text="NO SE DETECTO CÓDIGO")
+
+def inicia():
+    t = threading.Thread(target = screen_shoot())
+    t.start
 
 ventana = Tk()
 ventana.title('LECTOR DE CÓDIGOS QR')
@@ -33,14 +39,17 @@ ventana.geometry("520x220")
 ventana.configure(background = "light blue")
 file_name=""
 
-etiElement = Label(ventana, text="CARGAR ARCHIVO PNG O JPG",width=71,bg='light blue')
-etiElement.place(x=9,y=65)
+#etiElement = Label(ventana, text="CARGAR ARCHIVO PNG O JPG",width=71,bg='light blue')
+#etiElement.place(x=9,y=65)
+display=scrolledtext.ScrolledText(ventana,width=66,foreground='black',height=1,padx=10, pady=10,font=('Arial', 10))
+display.place(x=9,y=50)
 
 btnCargar = Button(ventana, text="CARGAR CÓDIGO",bg="khaki",width=22,command=abrir)
 btnCargar.place(x=178,y=110)
-btnScreen = Button(ventana, text="DETECTAR QR EN PANTALLA",bg="khaki",command=screen_shoot)
+btnScreen = Button(ventana, text="DETECTAR QR EN PANTALLA",bg="khaki",command=inicia)
 btnScreen.place(x=178,y=155)
 
 ventana.mainloop()
+
 
 
