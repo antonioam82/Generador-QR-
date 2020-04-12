360f5a17d8c47ae7faefff16b7b091fc1b859a97
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-04-12 22:37:24 +0200

    Update QR_DecoderGUI.py

--- QR_DecoderGUI.py
+++ QR_DecoderGUI.py
@@ -10,40 +10,123 @@ import numpy as np
 import cv2
 import os
 
-def abrir():
-    ruta = filedialog.askopenfilename(initialdir="/",title="SELECCIONAR ARCHIVO",
+class main:
+    def __init__(self):
+        self.ventana = Tk()
+        self.ventana.title('LECTOR DE CÓDIGOS QR')
+        self.ventana.geometry("520x235")
+        self.ventana.configure(background = "SlateGray2")
+        self.file_name=""
+        self.arc=""
+        #cap = cv2.VideoCapture(0)
+
+        self.display=scrolledtext.ScrolledText(self.ventana,width=66,foreground='black',height=3,padx=10, pady=10,font=('Arial', 10))
+        self.display.place(x=9,y=50)
+
+        self.btnCargar = Button(self.ventana, text="CARGAR CÓDIGO",bg="khaki",width=22,command=self.abrir)
+        self.btnCargar.place(x=178,y=140)
+        self.btnScreen = Button(self.ventana, text="DETECTAR QR EN PANTALLA",bg="khaki",command=self.screen_shot)
+        self.btnScreen.place(x=178,y=175)
+        self.btnCamara = Button(self.ventana, text="USAR CAMARA",bg="khaki",width=22,command=self.inicia_camara)
+        self.btnCamara.place(x=178,y=205)
+
+        self.ventana.mainloop()
+    def abrir(self):
+        ruta = filedialog.askopenfilename(initialdir="/",title="SELECCIONAR ARCHIVO",
                     filetypes =(("png files","*.png") ,("jpg files","*.jpg")))
-    if ruta != "":
-        archivo = cv2.imread(ruta)
+        if ruta != "":
+            archivo = cv2.imread(ruta)
+            info = decode(archivo)
+            if info != []:
+                self.display.delete('1.0',END)
+                self.display.insert(END,info[0][0])
+            else:
+                messagebox.showwarning("ERROR","NO SE DETECTÓ CÓDIGO")
+    def screen_shot(self):
+        pyautogui.screenshot("QRsearch_screenshoot.jpg")
+        archivo = cv2.imread("QRsearch_screenshoot.jpg")
         info = decode(archivo)
         if info != []:
-            display.delete('1.0',END)
-            display.insert(END,info[0][0])
+            self.display.delete('1.0',END)
+            self.display.insert(END,info[0][0])
+        else:
+            messagebox.showwarning("QR NO ENCONTRADO","NO SE DETECTÓ CÓDIGO")
+        os.remove("QRsearch_screenshoot.jpg")
+        
+    def visor(self):
+        ret, frame=self.vid.get_frame()
+        if ret:
+            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.canvas.create_image(0,0,image=self.photo,anchor=NW)#0,0
+            self.camara.after(15,self.visor)
+            
+    def inicia_camara(self):
+        t = threading.Thread(target=App())
+        t.start()
 
-def screen_shot():
-    pyautogui.screenshot("QRsearch_screenshoot.jpg")
-    archivo = cv2.imread("QRsearch_screenshoot.jpg")
-    info = decode(archivo)
-    if info != []:
-        display.delete('1.0',END)
-        display.insert(END,info[0][0])
-    else:
-        messagebox.showwarning("QR NO ENCONTRADO","NO SE DETECTÓ CÓDIGO")
-    os.remove("QRsearch_screenshoot.jpg")
+class App:
+    def __init__(self,font_video=0):
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter('output.avi',self.fourcc, 20.0, (640,480))
+        self.appName = "camera"
+        self.camara = Toplevel()
+        self.camara.title(self.appName)
+        self.camara['bg']='black'
+        self.font_video=font_video
+        self.vid=VideoCaptura(self.font_video)#!!!!!!!!!!!!!!!!!!!!!!!!!
+        self.label=Label(self.camara,text=self.appName,font=15,bg='blue',
+                         fg='white').pack(side=TOP,fill=BOTH)
+        
+        self.canvas=Canvas(self.camara,bg='red',width=self.vid.width,height=self.vid.height)
+        self.canvas.pack()
+        self.btnScreenshot = Button(self.camara,text="Screenshot",width=30,bg='goldenrod2',
+                    activebackground='red')
+        self.btnScreenshot.pack(side=TOP)
+        self.visor()
+        self.camara.mainloop()
+        
+    def captura(self):
+        ver,frame=self.vid.get_frame()
+        if ver:
+            image="IMG-"+time.strftime("%H-%M-%S-%d-%m")+".jpg"
+            cv2.imwrite(image,cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+            
+    def visor(self):
+        ret, frame=self.vid.get_frame()
+        #self.real_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if ret:
+            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.canvas.create_image(0,0,image=self.photo,anchor=NW)#0,0
+            self.camara.after(15,self.visor)
+#-------------------------------------------------------------------------------------------------------------------------            
 
-ventana = Tk()
-ventana.title('LECTOR DE CÓDIGOS QR')
-ventana.geometry("520x235")
-ventana.configure(background="SlateGray2")
-file_name=""
+class VideoCaptura:
+    def __init__(self,font_video=0):
+        self.vid = cv2.VideoCapture(font_video)
+        if not self.vid.isOpened():
+            raise ValueError("No se puede usar esta camara")
+        self.width=self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height=self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        
+    def get_frame(self):
+        if self.vid.isOpened():
+            verif,frame=self.vid.read()
+            if verif:
+                return(verif,cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return(verif,None)
+        else:
+            return(verif,None)
+        
 
-display = scrolledtext.ScrolledText(ventana,width=66,foreground='black',height=3,padx=10, pady=10,font=('Arial', 10))
-display.place(x=9,y=50)
-btnCargar = Button(ventana, text="CARGAR CÓDIGO",bg="khaki",width=22,command=abrir).place(x=178,y=140)
-btnScreen = Button(ventana, text="DETECTAR QR EN PANTALLA",bg="khaki",command=screen_shot).place(x=178,y=175)
-btnCamara = Button(ventana, text="USAR CAMARA",bg="khaki",width=22).place(x=178,y=205)
-
-ventana.mainloop()
-
+    def __del__(self):
+        print("OK")
+        #if self.vid.isOpened():
+        self.vid.release()
+            #self.out.release()
+                
+if __name__=="__main__":
+    main()    
+        
 
 
