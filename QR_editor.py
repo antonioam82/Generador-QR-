f72e535b9121140144196b9a77b3a894b253daaa
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-11-27 11:45:51 +0100

    Update QR_editor.py

--- QR_editor.py
+++ QR_editor.py
@@ -14,7 +14,9 @@ class app():
         self.display=scrolledtext.ScrolledText(self.ventana,width=70,height=10,font=('Arial', 10))
         self.display.place(x=30,y=30)
         self.btnCreate = Button(self.ventana,text="CREATE CODE",bg="light green",width=15)
-        self.btnCreate.place(x=225,y=250)
+        self.btnCreate.place(x=225,y=240)
+        self.btnView = Button(self.ventana,text="VIEW CODE",bg="gold2",width=15)
+        self.btnView.place(x=615,y=30)
         self.ventana.mainloop()
 
 if __name__=="__main__":
