8a12a00a2f453437f2d7fd3a4deda36baa697673
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-11-27 11:18:39 +0100

    Update QR_editor.py

--- QR_editor.py
+++ QR_editor.py
@@ -10,10 +10,11 @@ class app():
         self.ventana = Tk()
         self.ventana.title("EDITOR QR")
         self.ventana.configure(bg='light blue',width=800,height=370)
-        
 
         self.display=scrolledtext.ScrolledText(self.ventana,width=70,height=10,font=('Arial', 10))
         self.display.place(x=30,y=30)
+        self.btnCreate = Button(self.ventana,text="CREATE CODE",bg="light green",width=15)
+        self.btnCreate.place(x=225,y=250)
         self.ventana.mainloop()
 
 if __name__=="__main__":
